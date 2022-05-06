784580476dc9b88431a3ed213884d45fba3d4ab7
ChanManChan <ngopal253@gmail.com>
2022-05-06 22:21:57 +0530

    messenger-content-init

--- client/src/pages/dashboard/messengerContent/MessengerContent.js
+++ client/src/pages/dashboard/messengerContent/MessengerContent.js
@@ -1,74 +1,77 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from "react";
 
-import Message from '../message/Message'
-import './MessengerContent.css'
+import Message from "../message/Message";
+import "./MessengerContent.css";
 
 const MessengerContent = ({ chosenChatDetails }) => {
-    const DUMMY_MESSAGES = [
-        {
-          _id: 1,
-          content: "hello",
-          sameAuthor: "false",
-          author: {
-            username: "Marek",
-          },
-          date: "22/01/2022",
-          sameDay: false,
-        },
-        {
-          _id: 2,
-          content: "hello once again",
-          sameAuthor: "true",
-          author: {
-            username: "Marek",
-          },
-          date: "22/01/2022",
-          sameDay: true,
-        },
-        {
-          _id: 3,
-          content: "hello third time",
-          sameAuthor: "true",
-          author: {
-            username: "Marek",
-          },
-          date: "23/01/2022",
-          sameDay: false,
-        },
-        {
-          _id: 4,
-          content: "hello response first time",
-          sameAuthor: false,
-          author: {
-            username: "John",
-          },
-          date: "23/01/2022",
-          sameDay: true,
-        },
-        {
-          _id: 5,
-          content: "hello response third time",
-          sameAuthor: true,
-          author: {
-            username: "John",
-          },
-          date: "24/01/2022",
-          sameDay: false,
-        }
-      ]
-      
-    useEffect(() => {
-        
-    }, [chosenChatDetails])
+  const DUMMY_MESSAGES = [
+    {
+      _id: 1,
+      content: "hello",
+      sameAuthor: "false",
+      author: {
+        username: "Marek",
+      },
+      date: "22/01/2022",
+      sameDay: false,
+    },
+    {
+      _id: 2,
+      content: "hello once again",
+      sameAuthor: "true",
+      author: {
+        username: "Marek",
+      },
+      date: "22/01/2022",
+      sameDay: true,
+    },
+    {
+      _id: 3,
+      content: "hello third time",
+      sameAuthor: "true",
+      author: {
+        username: "Marek",
+      },
+      date: "23/01/2022",
+      sameDay: false,
+    },
+    {
+      _id: 4,
+      content: "hello response first time",
+      sameAuthor: false,
+      author: {
+        username: "John",
+      },
+      date: "23/01/2022",
+      sameDay: true,
+    },
+    {
+      _id: 5,
+      content: "hello response third time",
+      sameAuthor: true,
+      author: {
+        username: "John",
+      },
+      date: "24/01/2022",
+      sameDay: false,
+    },
+  ]
 
-    return (
-        <section className='messengerContent'>
-            <div className='messengerHeader'>
-                <p>This is the beginning of your conversation with {chosenChatDetails.username}</p>
-                {DUMMY_MESSAGES.map(message => <Message message={message} key={message._id} />)}
-            </div>
-        </section>
-    )
+  useEffect(() => {}, [chosenChatDetails])
+
+  return (
+    <section className="messengerContent">
+      <div className="messengerHeader">
+        <p>
+          This is the beginning of your conversation with{" "}
+          {chosenChatDetails.username}
+        </p>
+        {DUMMY_MESSAGES.map((message) => (
+          <Message message={message} key={message._id} />
+        ))}
+      </div>
+    </section>
+  )
 }
 
 export default MessengerContent
